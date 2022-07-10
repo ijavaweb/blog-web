4a879cb7f13c1dc2e9e6200ae43bdce034d8a46c
LoveYuBoBo <956410141@qq.com>
2022-07-10 20:16:28 +0800

    modify config

--- src/api/apiService.tsx
+++ src/api/apiService.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 const apiService = axios.create ({
-    baseURL: "http://www.iweb.fun:8083",
+    baseURL: "http://8.219.102.98:8083",
     // baseURL: "http://localhost:8083",
     // headers: {"Content-Type":"application/json"},
     // withCredentials:false,
